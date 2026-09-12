98b686027907bf4a47e262f864350ea415ad00f1
lunacoded404 <arichansts@gmail.com>
2026-09-12 20:03:16 +0700

    update production api url

--- frontend/js/api/api.js
+++ frontend/js/api/api.js
@@ -1,5 +1,4 @@
-export const API_BASE_URL =
-    "http://127.0.0.1:8000/api";
+export const API_BASE_URL = "https://backend-food-delivery-services.onrender.com/api";
 
 
 export async function apiRequest(
