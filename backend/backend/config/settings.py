fecafc6441eb7de8705939f31a2ceab77eca41a7
lunacoded404 <arichansts@gmail.com>
2026-09-12 20:09:53 +0700

    allow production frontend origin

--- backend/backend/config/settings.py
+++ backend/backend/config/settings.py
@@ -153,7 +153,7 @@ CORS_ALLOWED_ORIGINS = [
     "http://127.0.0.1:5500",
     "http://localhost:5500",
 
-    "https://backend-food-delivery-services.onrender.com",
+    "https://food-delivery-services.onrender.com",
 ]
 
 # Email 
